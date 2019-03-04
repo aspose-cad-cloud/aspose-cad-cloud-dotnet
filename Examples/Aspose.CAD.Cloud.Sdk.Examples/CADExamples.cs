f6868e573a3a71adeefa02d8712cf9e5aa1c9676
sohail-aspose <muhammad.sohail@aspose.com>
2019-03-04 12:29:20 -0800

    Updated SDK Examples Project Directory

--- Examples/Aspose.CAD.Cloud.Sdk.Examples/CADExamples.cs
+++ Examples/Aspose.CAD.Cloud.Sdk.Examples/CADExamples.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aspose.CAD_Cloud_APIs_Examples
+namespace Aspose.CAD.Cloud.Sdk.Examples
 {
     class CADExamples
     {
