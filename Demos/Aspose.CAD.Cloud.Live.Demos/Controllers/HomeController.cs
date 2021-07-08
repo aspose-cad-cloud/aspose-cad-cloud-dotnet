9776dddc4962533f1cae9fe636d803216c5efbad
ShakeelFaiz <shakeel.faiz@aspose.com>
2021-07-08 15:27:16 +0500

    Converted Aspose.CAD Cloud Conversion App to .NET 5.

---
+++ Demos/Aspose.CAD.Cloud.Live.Demos/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Aspose.CAD.Cloud.Live.Demos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Aspose.CAD.Cloud.Live.Demos.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public override string Product => string.Empty;
+
+        public HomeController(IMemoryCache cache) : base(cache)
+        {
+        }
+
+        public IActionResult Default()
+        {
+            ViewBag.PageTitle = Resources["cadConversionPageTitle"];
+            ViewBag.MetaDescription = Resources["cadConversionMetaDescription"];
+
+            var model = new LandingPageModel(this);
+            return View(model);
+        }
+    }
+}
---
+++ Demos/Aspose.CAD.Cloud.Live.Demos/Services/AsposeCADCloudApiService.cs
@@ -0,0 +1,54 @@
+﻿using Aspose.CAD.Cloud.Sdk.Api;
+using Aspose.CAD.Cloud.Sdk.Model.Requests;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Aspose.CAD.Cloud.Live.Demos.Services
+{
+
+    public interface IAsposeCADCloudApiService
+    {
+        Stream Convert(Stream file, string fileName, string toFormat);
+    }
+
+    public class AsposeCADCloudApiService : IAsposeCADCloudApiService
+    {
+        public readonly CadApi CadCloudApi;
+
+        public AsposeCADCloudApiService(IConfiguration config)
+        {
+            string ClientId = config["AsposeCADUserData:ClientId"];
+            string ClientSecret = config["AsposeCADUserData:ClientSecret"];
+
+            CadCloudApi = new CadApi(appKey: ClientSecret, appSid: ClientId);
+        }
+
+        public Stream Convert(Stream file, string fileName, string toFormat)
+        {
+            UploadFileRequest uploadFileRequest = new UploadFileRequest()
+            {
+                Path = fileName,
+                File = file
+            };
+
+            CadCloudApi.UploadFile(uploadFileRequest);
+
+            GetDrawingSaveAsRequest getDrawingSaveAsRequest = new GetDrawingSaveAsRequest()
+            {
+                Name = fileName,
+                OutputFormat = toFormat
+            };
+
+            var convertResult = CadCloudApi.GetDrawingSaveAs(getDrawingSaveAsRequest);
+
+            DeleteFileRequest deleteFileRequest = new DeleteFileRequest()
+            {
+                Path = fileName
+            };
+
+            CadCloudApi.DeleteFile(deleteFileRequest);
+
+            return convertResult;
+        }
+    }
+}
